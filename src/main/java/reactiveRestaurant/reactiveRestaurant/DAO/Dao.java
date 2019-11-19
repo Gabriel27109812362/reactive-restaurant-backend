4d3d071e6e74e17d27388f34b62b748e993bc5a7
Maciej Szklarzewski <szklarzewski.maciej@gmail.com>
2019-11-19 19:45:36 +0100

    CB19REACTR-62 Poprawki do bazowego interfejsu DAO.

--- src/main/java/reactiveRestaurant/reactiveRestaurant/DAO/Dao.java
+++ src/main/java/reactiveRestaurant/reactiveRestaurant/DAO/Dao.java
@@ -3,16 +3,51 @@ package reactiveRestaurant.reactiveRestaurant.DAO;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Bazowy interfejs DAO (Data Access Object, obiekt dostępu do danych),
+ * określający sposób interakcji z bazą danych
+ *
+ * @param <T> klasa konkretnego komponentu, z którym pracujemy
+ */
 public interface Dao<T> {
-    List<T> getAll();
-    List<T> findT ();
 
+    /**
+     * Zapisuje obiekt do bazy
+     * @param t obiekt, który ma zostać zapisany
+     */
     void save (T t);
-    void update (T t, String[] params);
+
+    /**
+     * Zwraca obiekt z bazy na podstawie id
+     *
+     * @param id id obiektu
+     * @return obiekt pobrany z bazy
+     */
+    T findById(UUID id);
+
+    /**
+     * Zwraca z bazy wszystkie obiekty z danej tabeli (encji)
+     *
+     * @return lista wszystkich obiektów z bazy
+     */
+    List<T> findAll();
+
+    /**
+     * Aktualizuje wartości obiektu w bazie
+     *
+     * @param t obiekt ze zaktualizowanymi wartościami
+     */
+    void update (T t);
+
+    /**
+     * Usuwa obiekt z bazy
+     *
+     * @param t obiekt to usunięcia
+     */
     void remove(T t);
-    void add(T t);
-    void findById(UUID id);
-    void swap(T t1,T t2);
 
-
+    /**
+     * Usuwa z bazy wszystkie obiekty z danej tabeli (encji)
+     */
+    void removeAll();
 }
