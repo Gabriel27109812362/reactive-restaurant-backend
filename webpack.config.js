f1d766953637ed4efcbfd84ef38030c89fa5b66d
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-12-23 14:51:57 +0100

    Add firebase config to webpack CB19REACTR-83

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const webpack = require('webpack');
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
    entry: './src/index.js',
    mode: 'production',
    target: 'node',
+   externals: [
+      nodeExternals({
+         whitelist: ['express', 'dotenv', 'body-parser'],
+      })],
    output: {
       path: path.resolve(__dirname, 'build'),
       filename: 'index.js',
@@ -13,6 +18,7 @@ module.exports = {
       modules: ['src', 'node_modules'],
       alias: {
          controllers: path.resolve(__dirname, 'src/controllers'),
+         services: path.resolve(__dirname, 'src/services'),
       },
       extensions: ['.js'],
 
@@ -20,7 +26,15 @@ module.exports = {
    plugins: [
       new webpack.DefinePlugin({
          'process.env': {
-            'APP_PORT': JSON.stringify('3000'),
+            'APP_PORT': JSON.stringify('4000'),
+            'API_KEY': JSON.stringify(process.env.API_KEY),
+            'AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+            'DB_URL': JSON.stringify(process.env.DB_URL),
+            'PROJECT_ID': JSON.stringify('rr-be-microservice'),
+            'STORE_BUCKET': JSON.stringify(process.env.STORE_BUCKET),
+            'MESS_SEND_ID': JSON.stringify(process.env.MESS_SEND_ID),
+            'APP_ID': JSON.stringify(process.env.APP_ID),
+            'MEASURMENT_ID': JSON.stringify(process.env.MEASURMENT_ID),
          },
       }),
    ],
